fdb641e62f98b9c06b470566f520fc263ab405aa
sebas0811buitrago <86497666+sebas0811buitrago@users.noreply.github.com>
2024-10-31 11:33:31 -0500

    adding Agrid Wrapper

--- src/AgGridWrapper.tsx
+++ src/AgGridWrapper.tsx
@@ -1,5 +1,39 @@
-const AgGridWrapper = () => {
-  return <div>AgGridWrapper</div>;
+import { AgGridReact, AgGridReactProps } from 'ag-grid-react';
+
+const ROWHEIGTH: number = 28;
+
+const AgGridWrapper = <TData extends unknown>(
+  props: AgGridReactProps<TData>
+) => {
+  const {
+    suppressContextMenu = true,
+
+    suppressCellFocus = true,
+    cacheBlockSize = 100,
+    blockLoadDebounceMillis = 10,
+    maxConcurrentDatasourceRequests = 1,
+    overlayNoRowsTemplate = 'No Results Found',
+    rowSelection = 'single',
+    rowHeight = ROWHEIGTH,
+    ...restAgGridProps
+  } = props;
+
+  return (
+    <div className={'ag-theme-balham'}>
+      <AgGridReact<TData>
+        suppressContextMenu={suppressContextMenu}
+        rowSelection={rowSelection}
+        suppressCellFocus={suppressCellFocus}
+        cacheBlockSize={cacheBlockSize}
+        blockLoadDebounceMillis={blockLoadDebounceMillis}
+        maxConcurrentDatasourceRequests={maxConcurrentDatasourceRequests}
+        overlayNoRowsTemplate={overlayNoRowsTemplate}
+        columnMenu="legacy"
+        rowHeight={rowHeight}
+        {...restAgGridProps}
+      />
+    </div>
+  );
 };
 
 export default AgGridWrapper;
